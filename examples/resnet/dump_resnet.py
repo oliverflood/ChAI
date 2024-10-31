782b78694299a2691767e23344d6d4a096981f41
Shreyas Khandekar <60454060+ShreyasKhandekar@users.noreply.github.com>
2024-10-31 12:55:09 -0500

    Update dump_resnet.py
    Using `pretrained=True` is deprecated. Instead we provide a `weights` argument and use `ResNet50_Weights.DEFAULT`
    
    Also removed the Dummy modules that were for testing the module loading in ChAI
    
    Signed-off-by: Shreyas Khandekar <60454060+ShreyasKhandekar@users.noreply.github.com>

--- examples/resnet/dump_resnet.py
+++ examples/resnet/dump_resnet.py
@@ -1,11 +1,8 @@
-import torch.nn as nn
-import torch
 import sys
 import os
+from torchvision.models import resnet50, ResNet50_Weights
 
-from resnet import resnet50
-
-model = resnet50(pretrained=True, progress=True, num_classes=1000)
+model = resnet50(weights=ResNet50_Weights.DEFAULT, progress=True, num_classes=1000)
 
 # Add the scripts directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'scripts')))
@@ -14,31 +11,3 @@ import chai
 
 model.chai_dump('models/resnet50','resnet50', with_json=False, verbose=True)
 
-
-
-class Dummy(nn.Module):
-    def __init__(self):
-        super(Dummy, self).__init__()
-
-        self.model = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=True),
-        )
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-
-
-
-class DummyTwo(nn.Module):
-    def __init__(self, input_model):
-        super(DummyTwo, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.newmod = nn.Sequential(
-            input_model,
-        )
-
-dummy = Dummy()
-
-dummy_two = DummyTwo(dummy)
-
-dummy.chai_dump('models/dummy', 'dummy', with_json=True, verbose=True)
-dummy_two.chai_dump('models/dummy_two', 'dummy_two', with_json=True, verbose=True)
