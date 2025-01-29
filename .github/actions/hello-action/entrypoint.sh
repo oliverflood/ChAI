caaacfcd4f5f2afcf880c0a277c1eb3af302b8c3
Iain Moncrief <moncrief@oregonstate.edu>
2025-01-28 21:29:59 -0800

    Testing docker.

--- .github/actions/hello-action/entrypoint.sh
+++ .github/actions/hello-action/entrypoint.sh
@@ -3,3 +3,11 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+
+stdout=$(which python3)
+echo "python3=$stdout" >> $GITHUB_OUTPUT
+
+
+stdout=$(which chpl)
+echo "chapel=$stdout" >> $GITHUB_OUTPUT
+
