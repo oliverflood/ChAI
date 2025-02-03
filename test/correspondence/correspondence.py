0393a77b9e9bea4ab4bf8cac53b2282a1757b3b8
Brandyn Tucknott <tucknotb@oregonstate.edu>
2025-02-02 18:00:13 -0800

    Uncommented last line in correspondence.py.

--- test/correspondence/correspondence.py
+++ test/correspondence/correspondence.py
@@ -46,7 +46,7 @@ def compile_chapel(test_name,test_path,chai_path):
     assert test_name == test_path.name
     chapel_test_path = test_path / f'{test_name}.chpl'
     test_dir = chapel_test_path.parent
-    os.system(f'chpl {chapel_test_path} -M {chai_path / 'lib'} -o {test_dir / test_name}')
+    os.system(f'chpl {chapel_test_path} -M {chai_path / "lib"} -o {test_dir / test_name}')
 
 
 def run_chapel_test(test_name,test_path):
@@ -116,4 +116,4 @@ for test in tests:
     else:
         print('[passed]', test_name)
 
-    # python_test_path = test['absolute_path'] / f'{test_name}.py'
+    python_test_path = test['absolute_path'] / f'{test_name}.py'
