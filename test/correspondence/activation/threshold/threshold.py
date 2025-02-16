6a079208221c50a87f2de58744346b2b72383415
Brandyn Tucknott <tucknotb@oregonstate.edu>
2025-02-15 21:02:42 -0800

    Fix python compilation issue in threshold test.

--- test/correspondence/activation/threshold/threshold.py
+++ test/correspondence/activation/threshold/threshold.py
@@ -3,36 +3,7 @@ import torch
 def test(imports):
     print = imports['print_fn']
 
-    a = torch.nn.Threshold()(torch.zeros(2,3))
-    print(a)
-
-    b = torch.nn.Threshold()(torch.zeros(2,3,4) - 60.0)
-    print(b)
-
-    c = torch.nn.Threshold()(torch.zeros(10) + 40.0)
-    print(c)
-    
-    # same values with threshold = -10.0
-    a = torch.nn.Threshold(threshold=-10.0)(torch.zeros(2,3))
-    print(a)
-
-    b = torch.nn.Threshold(threshold=-10.0)(torch.zeros(2,3,4) - 60.0)
-    print(b)
-
-    c = torch.nn.Threshold(threshold=-10.0)(torch.zeros(10) + 40.0)
-    print(c)
-    
-    # same values with value = 10.0
-    a = torch.nn.Threshold(value=10.0)(torch.zeros(2,3))
-    print(a)
-
-    b = torch.nn.Threshold(value=10.0)(torch.zeros(2,3,4) - 60.0)
-    print(b)
-
-    c = torch.nn.Threshold(value=10.0)(torch.zeros(10) + 40.0)
-    print(c)
-    
-    # same values with threshold = -50.0, value = 30.0
+    # threshold has no default values
     a = torch.nn.Threshold(threshold=-50.0, value=30.0)(torch.zeros(2,3))
     print(a)
 
@@ -41,3 +12,15 @@ def test(imports):
 
     c = torch.nn.Threshold(threshold=-50.0, value=30.0)(torch.zeros(10) + 40.0)
     print(c)
+    
+    a = torch.nn.Threshold(threshold=5.0, value=0.0)(torch.zeros(2,3))
+    print(a)
+
+    b = torch.nn.Threshold(threshold=-5.0, value=0.0)(torch.zeros(2,3,4) - 60.0)
+    print(b)
+
+    c = torch.nn.Threshold(threshold=-5.0, value=0.0)(torch.zeros(10) + 40.0)
+    print(c)
+    
+    a = torch.nn.Threshold(threshold=2.0, value=2.0)(torch.zeros(10) + 40.0)
+    print(a)
